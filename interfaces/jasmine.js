435b1f5c432769b1821e3547059f91a9d96451b1
Xavier Via <fernando.via@gmail.com>
2016-07-13 21:10:59 +0200

    Update Jasmine interfaces and add Sinon one

--- interfaces/jasmine.js
+++ interfaces/jasmine.js
@@ -1,2 +1,17 @@
-declare var describe: () => void;
-declare var it: () => void;
+// Simplified Jasmine type definitions, taken from
+// https://github.com/flowtype/flow-typed/blob/master/definitions/npm/jasmine_v2.4.x/flow_all/jasmine_v2.4.x.js#L21
+declare function describe(): any;
+declare function fdescribe(): any;
+declare function xdescribe(): any;
+
+declare function beforeEach(): any;
+declare function afterEach(): any;
+declare function beforeAll(): any;
+declare function afterAll(): any;
+
+declare function it(): any;
+declare function fit(): any;
+declare function xit(): any;
+
+declare function expect(): any;
+declare function spyOn(): any;
